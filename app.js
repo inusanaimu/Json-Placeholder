852f7112bd4e2da6a018ffae4508dd5bdf3c6c91
inusa naimu <inusanaimu@gmail.com>
2022-07-08 23:34:22 +0100

    Js Card dynamically created

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
+const url = 'https://jsonplaceholder.typicode.com/comments'
 window.addEventListener('DOMContentLoaded', () => {
   console.log('DOM is fully loaded, fire fetch request!')
 })
 
-let rootDiv = document.getElementById('root')
-console.log(rootDiv)
+const rootDiv = document.getElementById('root')
 
-const url = 'https://jsonplaceholder.typicode.com/comments'
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => console.log(data))
+function s() {
+  rootDiv.innerHTML = ''
+  for (let i = 1; i <= 2; i++) {
+    const content = document.createElement('div')
+    const topic = document.createElement('div')
+    const topicTopic = document.createElement('h4')
+    const topicBody = document.createElement('span')
+    const contentBody = document.createElement('div')
+
+    content.setAttribute('class', 'content')
+    topic.setAttribute('class', 'topic')
+    topicTopic.setAttribute('class', 'topicTopic')
+    topicBody.setAttribute('class', 'topicBody')
+    contentBody.setAttribute('class', 'contentBody')
+
+    topicTopic.innerHTML += `Topic${i}:`
+    topicBody.innerHTML = ' some'
+    contentBody.innerHTML += 'Body:'
+
+    topicTopic.appendChild(topicBody)
+    content.append(topic)
+    content.append(contentBody)
+    topic.append(topicTopic)
+    rootDiv.append(content)
+
+    content.addEventListener('click', (e) => {
+      topic.classList.toggle('border')
+      contentBody.classList.toggle('hide')
+    })
+  }
+}
+
+console.log(content)
+// fetch(url)
+//   .then((res) => res.json())
+//   .then((data) => console.log(data))
